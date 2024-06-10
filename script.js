d5c8e6b8cc5e2bec0b0d65d966f1ec8fd98c564d
aquinobr2023 gaspary <victor.gaspary@eaportal.org>
2024-06-10 11:17:10 +0000

    Projeto Arrumado

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
 import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
-
 const body = document.querySelector("body")
-
 botaoTema.addEventListener("click",  () => {
-
     trocarTema(body, botaoTema)
 })
 
 verificarTema(body, botaoTema)
-
 const botoesAssunto = document.querySelectorAll(".assuntos button")
-
 botoesAssunto.forEach(botao => {
-
     botao.addEventListener("click", selecionarAssunto)
 })
 
 function selecionarAssunto(evento) {
-
     const classeBotao = evento.target.className
-
     const assunto = document.querySelector(`.${classeBotao} span`).innerHTML
-
     localStorage.setItem("assunto", assunto)
-
     window.location.href = "./pages/quiz/quiz.html"
 }
